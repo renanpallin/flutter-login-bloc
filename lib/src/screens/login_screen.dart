fdd83aea5c5720d4c034b0979b175b31899970fe
Renan Pallin <renanpallin@gmai.com>
2019-01-09 19:49:41 -0200

    Singleton bloc

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 25),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+            hintText: 'password',
+            labelText: 'password',
+            errorText: snapshot.error
+          ),
+          obscureText: false,
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
